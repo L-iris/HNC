49587d896536f8353c4092c31919b643bcc9378f
zhuyang <saltwaterroomm@foxmail.com>
2021-11-05 13:37:23 +0800

    Init

---
+++ src/main/java/org/hust/cs/test/TestDependencies.java
@@ -0,0 +1,11 @@
+package org.hust.cs.test;
+
+import io.netty.channel.socket.DatagramPacket;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class TestDependencies {
+    public static void main(String[] args) {
+        log.debug("{}","Hello");
+    }
+}
---
+++ src/test/java/org/hust/cs/AppTest.java
@@ -0,0 +1,20 @@
+package org.hust.cs;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
